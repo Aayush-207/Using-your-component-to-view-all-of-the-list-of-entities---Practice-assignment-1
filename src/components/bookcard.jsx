5b6cd5c49b03afb90b8f30e290e1a38465ebba70
Aayush-207 <aayushraut2006@gmail.com>
2025-02-27 11:29:27 +0530

    -

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,25 @@
-// write the book component code here
+import React from 'react';
+import PropTypes from 'prop-types';
+import './bookcard.css';
+
+const BookCard = ({ book }) => {
+  return (
+    <div className="book-card">
+      <img src={book.image} alt={book.name} className="book-image" />
+      <h2>{book.name}</h2>
+      <p>{book.author}</p>
+      <p>{book.genre}</p>
+    </div>
+  );
+};
+
+BookCard.propTypes = {
+  book: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default BookCard;
